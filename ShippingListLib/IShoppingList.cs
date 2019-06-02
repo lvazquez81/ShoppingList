6c8fa86f2a705721d6bcad00ab6fd7267124bcf7
Luis Vázquez <lVazquez@worldremit.com>
2019-06-02 12:42:21 -0600

    Fix solution folder to include shipping list library.

---
+++ ShippingListLib/IShoppingList.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace ShippingListLib
+{
+    /// <summary>
+    /// Basic shopping list operations
+    /// </summary>
+    public interface IShoppingList
+    {
+        /// <summary>
+        /// Add item to our list
+        /// </summary>
+        int AddItem(string itemName);
+
+        /// <summary>
+        /// See what is in the list
+        /// </summary>
+        IList<string> GetList();
+
+        /// <summary>
+        /// Remove any existing item
+        /// </summary>
+        bool RemoveItem(int itemId);
+    }
+}
---
+++ ShippingListLib/VolatileShoppingList.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace ShippingListLib
+{
+    /// <summary>
+    /// Basic shopping list implementation with internal collection
+    /// </summary>
+    public class VolatileShoppingList : IShoppingList
+    {
+        private readonly IList<string> _list;
+
+        public VolatileShoppingList()
+        {
+            _list = new List<string>();
+        }
+
+        public int AddItem(string itemName)
+        {
+            _list.Add(itemName);
+            return _list.Count;
+        }
+
+        public IList<string> GetList()
+        {
+            return _list;
+        }
+
+        public bool RemoveItem(int itemId)
+        {
+            if (itemId > 0 && itemId <= _list.Count)
+            {
+                _list.RemoveAt(itemId - 1);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
