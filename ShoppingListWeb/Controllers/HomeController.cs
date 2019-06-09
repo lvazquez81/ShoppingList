76ebea594180b6cc35c9fcb2f7ff15f7980d8eac
Luis Vázquez <lVazquez@worldremit.com>
2019-06-09 17:46:26 -0600

    Skeleton web front-end

---
+++ ShoppingListWeb/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShoppingListWeb.Models;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace ShoppingListWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            IList<MyItem> items = new List<MyItem>()
+            {
+                new MyItem(1, "Eggs"),
+                new MyItem(2, "Bacon")
+            };
+
+            return View(items);
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        public IActionResult Delete()
+        {
+            IList<MyItem> items = new List<MyItem>()
+            {
+                new MyItem(1, "Eggs"),
+                new MyItem(2, "Bacon")
+            };
+
+            return View(items);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ShoppingListWeb/Models/MyItem.cs
@@ -0,0 +1,15 @@
+﻿namespace ShoppingListWeb.Models
+{
+    public class MyItem
+    {
+        public int Id { get; set; }
+
+        public string Item { get; set; }
+
+        public MyItem(int id, string item)
+        {
+            this.Id = id;
+            this.Item = item;
+        }
+    }
+}
